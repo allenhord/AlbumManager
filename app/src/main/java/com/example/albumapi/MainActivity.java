2dd343cc17f46b532b4e4bc712d1d0b9b5efa66e
cs_ahord001 <ahord001@odu.edu>
2025-03-29 18:14:14 -0500

    Added DBManager, DataModel

--- app/src/main/java/com/example/albumapi/MainActivity.java
+++ app/src/main/java/com/example/albumapi/MainActivity.java
@@ -10,41 +10,45 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseManager dbManager;
+    private TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-        return true;
+        resultView = findViewById(R.id.res);
+        dbManager = new DatabaseManager(this);
     }
 
     public void onClickShowDetails(View view) {
-        // inserting complete table details in this text field
-        TextView resultView = (TextView) findViewById(R.id.res);
+        List<DataModel> albumList = dbManager.getAllAlbums();
 
-        // creating a cursor object of the
-        // content URI
-        Cursor cursor = getContentResolver().query(Uri.parse("content://com.demo.album.provider/albums"), null, null, null, null);
-
-        // iteration of the cursor
-        // to print whole table
-        if (cursor.moveToFirst()) {
-            StringBuilder strBuild = new StringBuilder();
-            while (!cursor.isAfterLast()) {
-                strBuild.append("\n" + cursor.getString(cursor.getColumnIndex("id"))
-                        + "-" + cursor.getString(cursor.getColumnIndex("artist"))
-                        + "-" + cursor.getString(cursor.getColumnIndex("name")));
-                cursor.moveToNext();
+        if (!albumList.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for (DataModel album : albumList) {
+                sb.append(album.toString()).append("\n");
             }
-            resultView.setText(strBuild);
+            resultView.setText(sb.toString());
         } else {
             resultView.setText("No Records Found");
         }
